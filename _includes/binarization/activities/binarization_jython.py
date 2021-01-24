7f191e633f66b89b7eb4bd88b191d8ea4a2b11bb
Antonio Politi <mail@apoliti.de>
2021-01-24 16:34:45 +0100

    binarization exercise

--- _includes/binarization/activities/binarization_jython.py
+++ _includes/binarization/activities/binarization_jython.py
@@ -1,7 +1,7 @@
 from ij import IJ, ImagePlus
 from ij.plugin import Thresholder
 
-inputImage = IJ.openImage("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_two_cells.tif") 
+inputImage = IJ.openImage("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_two_cells.tif")
 IJ.setRawThreshold(inputImage, 44, 255, None)
 binaryImage = ImagePlus('Binary image 2 nuclei', Thresholder.createMask(inputImage))
 binaryImage.show()
