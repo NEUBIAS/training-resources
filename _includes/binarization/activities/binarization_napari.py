1caebeae32a09f074e310b2d18b616526fd1820a
Christian Tischer <christian.tischer@embl.de>
2021-11-30 10:00:09 +0100

    Improve script

--- _includes/binarization/activities/binarization_napari.py
+++ _includes/binarization/activities/binarization_napari.py
@@ -1,12 +1,14 @@
 # Image binarization for napari terminal window
 import imageio
 image = imageio.imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__two_cells.tif')
+
 # binarise
 binary_image = image > 49
+
 # show images
 viewer.add_image(image)
 viewer.add_image(binary_image)
-#
+
 # advanced/optional: check image data types
 image.dtype 
 binary_image.dtype
