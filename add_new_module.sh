f2fd90d806e26ec71b1ddf85726d1dd4ccdf8113
Christian Tischer <christian.tischer@embl.de>
2022-02-15 18:34:41 +0100

    Add template module and improve add_new_module script

--- add_new_module.sh
+++ add_new_module.sh
@@ -1,7 +1,7 @@
-cp -n _modules/binarization.md _modules/$1.md
+cp -n _modules/template.md _modules/$1.md
 mkdir _includes/$1
 mkdir _includes/$1/activities
 mkdir _includes/$1/exercises
-cp -n _includes/binarization/activities/binarization_imagejgui.md _includes/$1/activities/$1_imagejgui.md
-cp -n _includes/binarization/exercises/binarization_imagejgui.md _includes/$1/exercises/$1_imagejgui.md
+touch _includes/$1/activities/$1.md
+touch _includes/$1/exercises/$1.md
 
