0c3ffd04c144e71a5672d91ce8d8f64a824ca4b1
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-12-07 16:53:44 +0100

    Update pixels_skimage_napari activity.

--- _includes/pixels/activities/pixels_skimage_napari.py
+++ _includes/pixels/activities/pixels_skimage_napari.py
@@ -6,39 +6,37 @@
 ## pip install matplotlib
 #######################################################
 
-# Import python packages.
 from skimage.io import imread
-from napari.viewer import Viewer
-import matplotlib.pyplot as plt
-import numpy as np
-
 # Load the image.
 # You can also load a local image by providing the path to the file.
 image = imread("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_noisy_different_intensity.tif")
 
+# Print image shape
+print(image.shape)
+
 # Print the image pixel values.
 print(image)
 
-# Top left corner is [y, x] = [0, 0]
+# Top left corner is [y, x] = [r, c] = [0, 0]
 print(image[0, 0])
 
-# [y, x] = [1, 0]
+# [y, x] = [r, c] = [1, 0]
 print(image[1, 0])
 
-# [y, x] = [0, 2]
+# [y, x] = [r, c] = [0, 2]
 print(image[0, 2])
 
+from napari.viewer import Viewer
 # Create a new napari viewer.
 napari_viewer = Viewer()
 
 # Add an image to the napari_viewer.
 napari_viewer.add_image(image)
 
-# Get the image data type
-print(image.dtype)
+import numpy as np
+# Compute min and max.
+print(image.min(), image.max())
 
-# Use numpy.iinfo to get min and max values of a data type.
-np.iinfo(image.dtype)
-
+import matplotlib.pyplot as plt
 # Use matplotlib to quickly plot a histogram.
-plt.hist(image.flatten(), bins=256);
+plt.hist(image.flatten(), bins=np.arange(image.min(), image.max() + 1));
