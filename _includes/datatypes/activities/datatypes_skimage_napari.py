72beb14e6deb208334fdcbba4b9a44e0a78e9f9e
Christian Tischer <christian.tischer@embl.de>
2022-12-08 10:55:42 +0100

    Implement discussed changes

--- _includes/datatypes/activities/datatypes_skimage_napari.py
+++ _includes/datatypes/activities/datatypes_skimage_napari.py
@@ -1,23 +1,25 @@
+# Import modules
 import napari
+import numpy as np
+
+# Open napari
 viewer = napari.Viewer()
 
-# Read the intensity image
+# Open the image
+# - Uncomment the image that you want to read
 from skimage.io import imread
 image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_noisy_different_intensity.tif')
+#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_a.tif')
+#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_b.tif')
+#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__two_objects.tif')
+#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__autophagosomes.tif')
+#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__scanR_datatype_issue.tif')
 
 # View the image
 viewer.add_image(image)
 
-# Check the image's datatype
-print(image.dtype)
+# Check the image's datatype and its value limits
+print(np.iinfo(image.dtype))
 
 # Check the image's minimum and maximum intensity
-import numpy as np
 print(np.min(image),np.max(image))
-
-# Repeat the above steps for the other images
-# (Note that we do not need to import imread and numpy again)
-image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_a.tif')
-viewer.add_image(image)
-print(image.dtype)
-
