237b1284305a3d593009513c1740a604f68fe55f
Nikoula86 <nicola.gritti.86@gmail.com>
2022-12-09 13:49:36 +0100

    updated pixels and lut with load_from_url

--- _includes/lut/activities/explore_luts_skimage_napari.py
+++ _includes/lut/activities/explore_luts_skimage_napari.py
@@ -1,36 +1,38 @@
-# Instantiate the napari viewer
-import napari
-viewer = napari.Viewer()
+from load_from_url import load_from_url
+from napari.viewer import Viewer
+import numpy as np
 
 # Read the image
-from skimage.io import imread
-image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_high_dynamic_range.tif')
+image_url = load_from_url('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_high_dynamic_range.tif')
+print(image_url.dims)
+image = image_url.data
 
 # Check image type and values
-import numpy as np
 print(image.dtype, np.min(image), np.max(image))
 
+# Create a new napari viewer.
+napari_viewer = Viewer()
 # View the intensity image as grayscale
-viewer.add_image(image, name='image_grayscale', colormap='gray')
+napari_viewer.add_image(image, name='image_grayscale', colormap='gray')
 
 # Change brightness and contrast
-viewer.layers['image_grayscale'].contrast_limits=(100, 175)
+napari_viewer.layers['image_grayscale'].contrast_limits=(100, 175)
 # Napari GUI: explore different contrast limits
 
 # View the intensity image as grayscale
-viewer.add_image(image, name='image_grayscale2', colormap='gray')
+napari_viewer.add_image(image, name='image_grayscale2', colormap='gray')
 # Napari GUI: visualize images side by side
 # Napari GUI: change brightness and contrast to visualize dim nuclei
 
 # Check available colormap
 print(list(napari.utils.colormaps.AVAILABLE_COLORMAPS))
 # Change colormap
-viewer.add_image(image, name='image_turbo', colormap='turbo')
+napari_viewer.add_image(image, name='image_turbo', colormap='turbo')
 # Napari GUI: explore the LUTs
 
 # Extract image data from the layers
-image_grayscale = viewer.layers['image_grayscale'].data
-image_grayscale2 = viewer.layers['image_grayscale2'].data
+image_grayscale = napari_viewer.layers['image_grayscale'].data
+image_grayscale2 = napari_viewer.layers['image_grayscale2'].data
 
 # Compare raw data
 print(image_grayscale[0:5,0:5])
--- functions/load_from_url.py
+++ functions/load_from_url.py
@@ -1,24 +1,29 @@
 import os, tempfile
-import urllib
+import requests
 from aicsimageio import AICSImage
 from aicsimageio.readers import BioformatsReader
 
-def load_from_url(url):
+def load_from_url(url, verbose=False):
 
     with tempfile.TemporaryDirectory() as tempdir:
         # extract image name
         fname = os.path.join(tempdir, url.rsplit('/', 1)[1])
+
+        if verbose:
+            print('Saving file\n', fname, '\nin:\n', tempdir)
+
         # download image from url and save it with the same name
-        r = urllib.request.urlretrieve(url, fname)
+        r = requests.get(url)
+        open(fname, "wb").write(r.content)
         # load image using AICSImage to retrieve metadata
-        image = AICSImage(fname, reader=BioformatsReader)
-        _ = image.data # this is to make sure the image is loaded into memory
+        image_aics = AICSImage(fname, reader=BioformatsReader)
+        _ = image_aics.data # this is to make sure the image is loaded into memory
     
-    return image
+    return image_aics
 
 if __name__=='__main__':
     url = 'https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyz_8bit_calibrated__mri_full_head.tif'
-    image = load_from_url(url)
+    image = load_from_url(url, verbose=True)
     print(image.data)
     print(image.physical_pixel_sizes)
 
