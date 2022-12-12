73cbb85dd892a3ad356edd8798bd439c05eade9c
Nikoula86 <nicola.gritti.86@gmail.com>
2022-12-12 10:14:49 +0100

    added ics/ids loading

--- functions/load_from_url.py
+++ functions/load_from_url.py
@@ -7,19 +7,36 @@ def load_from_url(url, verbose=False):
 
     with tempfile.TemporaryDirectory() as tempdir:
         # extract image name
-        fname = os.path.join(tempdir, url.rsplit('/', 1)[1])
+        url_fname = url.rsplit('/', 1)[1]
+        file_name, file_ext = os.path.splitext(url_fname)
+        url_file_name, _ = os.path.splitext(url)
+        fname = os.path.join(tempdir, file_name+file_ext) 
 
         if verbose:
             print('Saving file\n', fname, '\nin:\n', tempdir)
 
-        # download image from url and save it with the same name
+        # download url file and save it with the same name
         r = requests.get(url)
         open(fname, "wb").write(r.content)
+        
+        # if file is ics, download corresponding ids
+        if file_ext=='.ics':
+            r = requests.get(url_file_name+'.ids')
+            ids_name = os.path.join(tempdir, file_name+'.ids')
+            open(ids_name, "wb").write(r.content)
+
+        # if file is xml, download corresponding h5
+        if file_ext=='.xml':
+            r = requests.get(url_file_name+'.h5')
+            ids_name = os.path.join(tempdir, file_name+'.h5')
+            open(ids_name, "wb").write(r.content)
+
         # load image using AICSImage to retrieve metadata
-        image_aics = AICSImage(fname, reader=BioformatsReader)
-        _ = image_aics.data # this is to make sure the image is loaded into memory
+        image = AICSImage(fname, reader=BioformatsReader)
+        _ = image.data # this is to make sure the image is loaded into memory
     
-    return image_aics
+    return image
+
 
 if __name__=='__main__':
     url = 'https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyz_8bit_calibrated__mri_full_head.tif'
