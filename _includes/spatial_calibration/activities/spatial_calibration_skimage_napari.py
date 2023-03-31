ee4fcfd6285739c9dccde93517e581ba0c6e4c5d
Nicola Gritti <gritti@embl.de>
2023-03-31 13:52:32 +0200

    created spatial calibration activity

--- _includes/spatial_calibration/activities/spatial_calibration_skimage_napari.py
+++ _includes/spatial_calibration/activities/spatial_calibration_skimage_napari.py
@@ -3,26 +3,105 @@
 ## installation activity for skimage napari.
 #######################################################
 
+#%%
 # Import python packages.
+import os, re, requests, tifffile
 from napari.viewer import Viewer
+import numpy as np
 
-from load_from_url import load_from_url
-# Read image with AICSImage.
-image = load_from_url("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyz_8bit__mitotic_plate_calibrated.tif")
+#%% ### download and read tif file
+# download example image
+image_url = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyz_8bit__mitotic_plate_calibrated.tif"
+image_fname = image_url.rsplit('/', 1)[1]
+r = requests.get(image_url)
+with open(os.path.join(image_fname), "wb") as f:
+    f.write(r.content)
 
-# Display information about dimensions.
-print(image.dims)
+with tifffile.TiffFile(image_fname) as tif:
+    array = tif.asarray()
+    meta = tif.pages[0]
+    y, x = [1 / item for item in meta.resolution]
+    z = float(re.findall('spacing=(\d*\.*\d+)', meta.description)[0])
+    voxel_size = [z, y, x]
 
-# Display physical pixel size read from the metadata.
-print(image.physical_pixel_sizes)
+# visualize metadata
+for tag in meta.tags:
+    tag_name, tag_value = tag.name, tag.value
+    print(tag_name,'---', tag_value)
 
-# Create a new napari_viewer
+#%%
+# Create a napari_viewer and visualize image
 napari_viewer = Viewer()
+napari_viewer.add_image(array, scale=voxel_size)
 
-# Add image with scaling to the viewer.
-napari_viewer.add_image(image.data, scale=image.physical_pixel_sizes)
-# Napari GUI: Change order of visible axes.
-# Napari GUI: Explor 3D visualization.
+#%%
+# update voxel size to some other values
+updated_voxel_size = [1.,1.,1.]#2 * item for item in input_resolution]
 
-# Note: As of now napari lacks funcitonality to easily measure distances between points in an open image.
-# Currently, no orthogonal view plugin exists for napari.
+# resave image
+tifffile.imwrite(
+    'resaved_image.tif',
+    array,
+    imagej = True,
+    metadata = {
+        'spacing':updated_voxel_size[0],
+        'unit':'um',
+        'axes':'ZYX',
+    },
+    resolution=(1./updated_voxel_size[1], 1./updated_voxel_size[2])
+)
+
+#%%
+with tifffile.TiffFile('resaved_image.tif') as tif:
+    array = tif.asarray()
+    meta = tif.pages[0]
+    y, x = [1 / item for item in meta.resolution]
+    z = float(re.findall('spacing=(\d*\.*\d+)', meta.description)[0])
+    updated_voxel_size = [z, y, x]
+
+for tag in meta.tags:
+    tag_name, tag_value = tag.name, tag.value
+    print(tag_name,'---', tag_value)
+
+#%%
+# Create a new napari_viewer and visualize image
+napari_viewer = Viewer()
+napari_viewer.add_image(array, scale=updated_voxel_size)
+
+# using the horthogonal views button (change order of visible axis), 
+# inspect the volume and observe that the voxel size makes sense.
+# use 3D viewer button to inspect data in 3D.
+
+#%%
+# extract point coordinates
+layer_names = [l.name for l in napari_viewer.layers]
+points2d = napari_viewer.layers[layer_names.index('points2D')].data
+points3d = napari_viewer.layers[layer_names.index('points3D')].data
+
+#%%
+# compute distance between points in voxel indices
+dist_2d_pxl = np.sqrt(((points2d[1]-points2d[0])**2).sum())
+print('Distance in pixels:',dist_2d_pxl)
+
+# calibrate point position
+points2d_cal = np.stack([p*voxel_size for p in points2d])
+
+# compute distance between points in um using calibrated, appreciate that these are different values!
+dist_2d_cal = np.sqrt(((points2d_cal[1]-points2d_cal[0])**2).sum())
+print('Distance in um:',dist_2d_cal)
+
+# appreciate that, in this special case, one case use voxel index distance multiplied y XY pxl size:
+print('Distance in um:',dist_2d_pxl * voxel_size[1])
+
+#%%
+# compute distance between points in voxel indices
+dist_3d_pxl = np.sqrt(((points3d[1]-points3d[0])**2).sum())
+print('Distance in pixels:',dist_3d_pxl)
+
+# Appreciate that this distance doesn't make any physical sense!
+# Instead, one should always measure distances between points using calibration!
+points3d_cal = np.stack([p*voxel_size for p in points3d])
+
+# compute distance between points in um using calibrated, appreciate that in this case it is important to do the calibration!
+dist_3d_cal = np.sqrt(((points3d_cal[1]-points3d_cal[0])**2).sum())
+print('Distance in um:',dist_3d_cal)
