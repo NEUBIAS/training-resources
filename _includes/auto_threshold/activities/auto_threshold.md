b85298e8a9d0a3ed3af237d18db35515c3bf0633
Sebastian Gonzalez Tirado <sebgoti8@gmail.com>
2023-03-31 12:08:33 +0200

    Finish skimage implementation for Activity 2 and rearange folder structure to have multiactivities instead of activitiy/exercise

--- _includes/auto_threshold/activities/auto_threshold.md
+++
@@ -1,6 +0,0 @@
-### Apply manual and automated thresholds
-  - Manual vs. auto thresholding
-    - Open [xy_8bit__nuclei_without_offset.tif](https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_without_offset.tif) and [xy_8bit__nuclei_with_offset.tif](https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_with_offset.tif)
-    - Explore the differences in doing manual and auto thresholding
-
-
--- _includes/auto_threshold/auto_threshold_act2_skimage_napari.py
+++ _includes/auto_threshold/auto_threshold_act2_skimage_napari.py
@@ -16,7 +16,13 @@ viewer.add_image(image, name='image')
 
 # Obtain threshold value using Otsu's algorithm
 from skimage.filters import threshold_otsu
-thr_otsu = threshold_otsu(image)
-thresholded = image > thr_otsu
+threshold_otsu = image > threshold_otsu(image)
 
-viewer.add_labels(thresholded, name='otsu_thr')
+viewer.add_labels(thresholded, name='otsu', num_colors=1, color={0: 'green'})
+# Explore the results in the napari viewer. For 3D data one can change the order 
+# of visible axes (bottom left in napari viewer window). If not satisfied by the
+# results, we can explore other threshold algorithms:
+from skimage.filters import threshold_li
+thresholded_li = image > threshold_li(image)
+
+viewer.add_labels(thresholded_li, name='li', num_colors=1, color={0: 'orange'})
