26c70de502a085c4c14015179dae9820f13b4ad9
grinic <45073015+grinic@users.noreply.github.com>
2023-04-25 16:59:01 +0200

    fluorescence intensity with numpy

--- _includes/measure_intensities/activities/measure_intensities_skimage_napari.py
+++ _includes/measure_intensities/activities/measure_intensities_skimage_napari.py
@@ -9,6 +9,7 @@ from OpenIJTIFF import open_ij_tiff, save_ij_tiff
 from skimage.io import imsave
 import numpy as np
 from napari.viewer import Viewer
+import pandas as pd
 
 #%% ### download and read tif file
 # load image from url
@@ -22,3 +23,33 @@ labels, axes_labels, voxel_labels, units_labels = open_ij_tiff(fpath)
 napari_viewer = Viewer()
 napari_viewer.add_image(image)
 napari_viewer.add_labels(labels)
+
+#%%
+# Measure the mean fluorescence intensity and the object pixel area manually
+objects_labels = list(set(labels.flatten()))
+objects_labels.sort()
+print(objects_labels)
+# Observe that there are 3 labels, 0 being the label of the background, 1,2 are the cells
+
+#%%
+# Use numpy to compute area and fluorescence intensities of objects
+mean_intensities = [None for i in objects_labels]
+areas = [None for i in objects_labels]
+
+i = 0
+for label in objects_labels:
+    
+    areas[i] = len(labels[labels==label])
+    mean_intensities[i] = np.mean(image[labels==label])
+    
+    i += 1
+    
+# Construct human-readable table
+df = pd.DataFrame({
+    'object_label': objects_labels,
+    'area': areas,
+    'mean_intensity': mean_intensities
+})
+
+#%%
+df
