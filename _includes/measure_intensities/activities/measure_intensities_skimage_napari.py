ea13eb852a111528da8766cd2e3400b6151800fc
grinic <45073015+grinic@users.noreply.github.com>
2023-04-28 14:10:43 +0200

    activity with regionprops_table

--- _includes/measure_intensities/activities/measure_intensities_skimage_napari.py
+++ _includes/measure_intensities/activities/measure_intensities_skimage_napari.py
@@ -10,8 +10,9 @@ from skimage.io import imsave
 import numpy as np
 from napari.viewer import Viewer
 import pandas as pd
+from skimage.measure import regionprops_table
 
-#%% ### download and read tif file
+#%%
 # load image from url
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__h2b.tif"
 image, axes_image, voxel_image, units_image = open_ij_tiff(fpath)
@@ -25,128 +26,75 @@ napari_viewer.add_image(image, name='image')
 napari_viewer.add_labels(labels, name='objects')
 
 #%%
-# Extract object labels
+# Measure the mean fluorescence intensity and the object pixel area manually
 objects_labels = np.unique(labels.flatten())
 objects_labels.sort()
 print(objects_labels)
-
-# Observe that there are 3 labels, 0 being the label of the background, 1,2 are the cells
-objects_labels = np.delete(objects_labels,0) # remove the background from the object labels
-print(objects_labels)
+# Observe that there are 3 values, 0 being the label of the background, 1,2 are the cells
 
 #%%
-# Use numpy to compute area and fluorescence intensities of objects
-mean_intensities = [None for i in objects_labels]
-areas = [None for i in objects_labels]
-
-i = 0
-for label in objects_labels:
+# use the intensity_image
+fluo_measures = pd.DataFrame(
+    regionprops_table(
+        labels,
+        intensity_image = image,
+        properties = ('label','area','intensity_mean','intensity_max')
+    )
+)
     
-    areas[i] = len(labels[labels==label])
-    mean_intensities[i] = np.mean(image[labels==label])
-    
-    i += 1
-    
-# Construct human-readable table
-df = pd.DataFrame({
-    'object_label': objects_labels,
-    'area (pixels)': areas,
-    'mean_intensity': mean_intensities
-})
-
-df.to_csv("object_measurements.csv")
+fluo_measures.to_csv("object_measurements.csv")
 
 #%%
-df
+fluo_measures
 
 #%%
 # Even though we could have an estimation of background intensity with the "0" label,
 # use the napari viewer to manually create an additional label (3 in this case)
-# and compute the background.
-# Append the background to the table
+# and the following code to measure the mean intensity inside.
 
-new_labels = napari_viewer.layers[1].data
-background = np.mean(image[new_labels==3])
+background = fluo_measures[fluo_measures.label==3].intensity_mean.values[0]
 
-df['background'] = background
-
-#%%
-df
-
-#%%
-# compute mean, max, sum intensity corrected by the background
-areas = [None for i in objects_labels]
-mean_intensities = [None for i in objects_labels]
-max_intensities = [None for i in objects_labels]
-sum_intensities = [None for i in objects_labels]
-
-i = 0
-for label in objects_labels:
-    
-    areas[i] = len(labels[labels==label])
-    mean_intensities[i] = np.mean(image[labels==label]) - background
-    max_intensities[i] = np.max(image[labels==label]) - background
-    sum_intensities[i] = np.sum(image[labels==label]) - background*areas[i]
-    
-    i += 1
-    
-df = pd.DataFrame({
-    'object_label': objects_labels,
-    'area (pixels)': areas,
-    'mean_corrected': mean_intensities,
-    'max_corrected': max_intensities,
-    'sum_corrected': sum_intensities,
-    'background': background,
-})
+# Append the background-corrected values to the table
+fluo_measures['intensity_sum'] = fluo_measures.intensity_mean * fluo_measures.area
+fluo_measures['mean_corr'] = fluo_measures.intensity_mean - background
+fluo_measures['max_corr'] = fluo_measures.intensity_max - background
+fluo_measures['sum_corr'] = fluo_measures.mean_corr * fluo_measures.area
 
 #%%
-df
+fluo_measures
 
 #%%
-### Perform the same analysis using slightly different labels
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_labels__h2b_dilate_labels.tif"
-large_labels, axes_labels, voxel_labels, units_labels = open_ij_tiff(fpath)
+dilated_labels, axes_labels, voxel_labels, units_labels = open_ij_tiff(fpath)
 
-# Add the new labels to the napari viewer
-napari_viewer.add_labels(large_labels, name='large_objects')
-
-# extract object labels
-objects_labels = np.unique(large_labels.flatten())
-objects_labels.sort()
-objects_labels = np.delete(objects_labels,0) # remove the background from the object labels
-
-# compute mean, max, sum intensity corrected by the background
-areas = [None for i in objects_labels]
-mean_intensities = [None for i in objects_labels]
-max_intensities = [None for i in objects_labels]
-sum_intensities = [None for i in objects_labels]
-
-i = 0
-for label in objects_labels:
-    
-    areas[i] = len(large_labels[large_labels==label])
-    mean_intensities[i] = np.mean(image[large_labels==label]) - background
-    max_intensities[i] = np.max(image[large_labels==label]) - background
-    sum_intensities[i] = np.sum(image[large_labels==label]) - background*areas[i]
-    
-    i += 1
-    
-# Construct human-readable table
-df_large = pd.DataFrame({
-    'object_label': objects_labels,
-    'area (pixels)': areas,
-    'mean_corrected': mean_intensities,
-    'max_corrected': max_intensities,
-    'sum_corrected': sum_intensities,
-    'background': background,
-})
-
-df_large
-
-# Observe that the quantification has changed. Discuss how and why.
+objects_labels = np.unique(dilated_labels.flatten())
+print(objects_labels)
 
 #%%
-# Try quantifying fluorescence inetnsity for a challenging image
+napari_viewer.add_labels(dilated_labels, name='dilated_objects')
+
+#%%
+# use the intensity_image to compute the fluorescence parameters
+fluo_measures_dilated = pd.DataFrame(
+    regionprops_table(
+        dilated_labels,
+        intensity_image = image,
+        properties = ('label','area','intensity_mean','intensity_max')
+    )
+)
+    
+# Append the background-corrected values to the table
+fluo_measures_dilated['intensity_sum'] = fluo_measures_dilated.intensity_mean * fluo_measures_dilated.area
+fluo_measures_dilated['mean_corr'] = fluo_measures_dilated.intensity_mean - background
+fluo_measures_dilated['max_corr'] = fluo_measures_dilated.intensity_max - background
+fluo_measures_dilated['sum_corr'] = fluo_measures_dilated.mean_corr * fluo_measures_dilated.area
+
+fluo_measures_dilated.to_csv("object_measurements_dilated.csv")
+
+#%%
+fluo_measures_dilated
+
+#%%
 # load image from url
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyc_16bit__embryo_transmission_fluorescence.tif"
 image, axes_image, voxel_image, units_image = open_ij_tiff(fpath)
@@ -158,4 +106,3 @@ napari_viewer1 = Viewer()
 napari_viewer1.add_image(image[0], name='brightfield')
 napari_viewer1.add_image(image[1], name='fluo', colormap='magenta', opacity=0.5)
 
-# Appreciate the low SNR and the inhomogeneous background, which makes quantification challenging.
