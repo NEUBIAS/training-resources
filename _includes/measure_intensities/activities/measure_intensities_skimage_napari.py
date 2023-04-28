0626c12a072f2454d986a00eb048adc724f067ba
Antonio Politi <mail@apoliti.de>
2023-04-28 17:46:02 +0200

    cell edits WIP

--- _includes/measure_intensities/activities/measure_intensities_skimage_napari.py
+++ _includes/measure_intensities/activities/measure_intensities_skimage_napari.py
@@ -1,9 +1,7 @@
-#######################################################
-## To follow along you need to complete the tool
-## installation activity for skimage napari.
-#######################################################
+# %% [markdown]
+# ### Imports and Napari start up
 
-#%%
+# %%
 # Import python packages.
 from OpenIJTIFF import open_ij_tiff, save_ij_tiff
 from skimage.io import imsave
@@ -12,67 +10,74 @@ from napari.viewer import Viewer
 import pandas as pd
 from skimage.measure import regionprops_table
 
-#%%
+# %%
 # load image from url
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__h2b.tif"
 image, axes_image, voxel_image, units_image = open_ij_tiff(fpath)
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_labels__h2b.tif"
 labels, axes_labels, voxel_labels, units_labels = open_ij_tiff(fpath)
 
-#%%
+# %%
 # Create a napari_viewer and visualize image and labels
 napari_viewer = Viewer()
 napari_viewer.add_image(image, name='image')
 napari_viewer.add_labels(labels, name='objects')
 
-# Even though we could have an estimation of background intensity with the "0" label,
-# use the napari viewer to manually create an additional label ("3" in this case)
+# %% [markdown]
+# ## Compute the intensities 
+# ### Background label  
+# We can have an estimation of background intensity with the "0" label
+# We can also manually create in the Viewer an additional label ("3" in this case)
 
-#%%
+# %%
 objects_labels = np.unique(labels.flatten())
 objects_labels.sort()
 print(objects_labels)
-# Observe that there are 3 values, 0 being the label of the background, 
-# 1,2 are the cells and 3 is the labeled background.
 
-#%%
+# %% [markdown]
+# Observe that there are 4 values, 0 being the label of the background, 
+# 1,2 are the cells and 3 is the manually labeled background.
+
+# %%
 # Use the intensity_image option to measure fluorescence intensity of objects
-fluo_measures = pd.DataFrame(
-    regionprops_table(
+fluo_regionprops = regionprops_table(
         labels,
         intensity_image = image,
         properties = ('label','area','intensity_mean','intensity_max')
-    )
 )
-    
+print(fluo_regionprops)
+
+# %%
+# Wrap in a panda dataframe for better visualization nd export
+fluo_measures = pd.DataFrame(fluo_regionprops)
 fluo_measures.to_csv("object_measurements.csv")
+print(fluo_measures)
 
-#%%
-fluo_measures
-
-#%%
+# %%
+# Compute background value
 background = fluo_measures[fluo_measures.label==3].intensity_mean.values[0]
 
+# %%
 # Append the background-corrected values to the table
 fluo_measures['intensity_sum'] = fluo_measures.intensity_mean * fluo_measures.area
 fluo_measures['mean_corr'] = fluo_measures.intensity_mean - background
 fluo_measures['max_corr'] = fluo_measures.intensity_max - background
 fluo_measures['sum_corr'] = fluo_measures.mean_corr * fluo_measures.area
 
-#%%
+# %%
 fluo_measures
 
-#%%
+# %%
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_labels__h2b_dilate_labels.tif"
 dilated_labels, axes_labels, voxel_labels, units_labels = open_ij_tiff(fpath)
 
 objects_labels = np.unique(dilated_labels.flatten())
 print(objects_labels)
 
-#%%
+# %%
 napari_viewer.add_labels(dilated_labels, name='dilated_objects')
 
-#%%
+# %%
 # Use the intensity_image to compute the fluorescence parameters
 fluo_measures_dilated = pd.DataFrame(
     regionprops_table(
@@ -90,10 +95,10 @@ fluo_measures_dilated['sum_corr'] = fluo_measures_dilated.mean_corr * fluo_measu
 
 fluo_measures_dilated.to_csv("object_measurements_dilated.csv")
 
-#%%
+# %%
 fluo_measures_dilated
 
-#%%
+# %%
 # load image from url
 fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyc_16bit__embryo_transmission_fluorescence.tif"
 image, axes_image, voxel_image, units_image = open_ij_tiff(fpath)
