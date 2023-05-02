be9f1c81adcdbbb3c0290d57880e9b4fe87fa9ea
Antonio Politi <mail@apoliti.de>
2023-05-02 16:46:39 +0200

    fixed py file

--- _includes/tool_installation/test_installation_skimage_napari.py
+++ _includes/tool_installation/test_installation_skimage_napari.py
@@ -1,5 +1,13 @@
+#####
+# test_installation_skimage_napari.py
+# If you have the package jupytext installed (pip install jupytext). 
+# Cells will be automatically generated.   
+#####
+
 # %% [markdown]
-# #Test python napari skimage install
+# # Test python napari skimage install
+
+# %%
 # Instantiate the napari viewer
 import napari
 viewer = napari.Viewer()
@@ -7,8 +15,16 @@ viewer = napari.Viewer()
 # %%
 # Read the intensity image
 from skimage.io import imread
-image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__two_cells.tif')
+fpath = 'https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__two_cells.tif'
+image = imread(fpath)
 
 # %%
 # View the intensity image
 viewer.add_image(image)
+
+# %% 
+# Read via OpenIJTIFF (OpenIJTiff.py must be in the same folder as the notebook path)
+from OpenIJTIFF import open_ij_tiff
+image2, axes_image, voxel_image, units_image = open_ij_tiff(fpath)
+# View the intensity image
+viewer.add_image(image2)
