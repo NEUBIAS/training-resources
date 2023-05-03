de3634777ec547e2aa58c26cd536d1da31106807
grinic <45073015+grinic@users.noreply.github.com>
2023-05-03 14:51:24 +0200

    changed imread to open_ij_tiff and regionprops to regionprops_table with pandas

--- _includes/workflow_segment_2d_nuclei_measure_shape/segment_2d_nuclei_skimage_napari.py
+++ _includes/workflow_segment_2d_nuclei_measure_shape/segment_2d_nuclei_skimage_napari.py
@@ -1,31 +1,43 @@
+# %%
 # Import modules
 import napari
-from skimage.io import imread
-from skimage.measure import label, regionprops
+from OpenIJTIFF import open_ij_tiff
+from skimage.measure import label, regionprops_table
+import pandas as pd
 
+# %%
 # Instantiate the napari viewer
-viewer = napari.Viewer()
+napari_viewer = napari.Viewer()
 
+# %%
 # Read and inspect the image:
-image_t1 = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__mitocheck_incenp_t1.tif')
-viewer.add_image(image_t1, name='frame 1')
+fpath = 'https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__mitocheck_incenp_t1.tif'
+image_t1, axes_t1, voxel_size_input_t1, units_t1 = open_ij_tiff(fpath)
 
-threshold = 25
+napari_viewer.add_image(image_t1, name='frame 1')
 
-def nuclei_quantification(image):
+
+# %%
+def nuclei_quantification(image, viewer, thr):
+    print(f"Threshold: {thr}")
     # Binarize the image:
-    image = image > threshold
+    image = image > thr
     viewer.add_labels(image, name='binary')
     # Find labels:
     img_labels = label(image)
     viewer.add_labels(img_labels, name='labels')
     # Obtain cell properties:
-    properties = regionprops(img_labels)
+    properties = regionprops_table(
+        img_labels,
+        properties = {'label', 'area'}
+    )
     # Print areas for each cell:
-    for cellid, region in enumerate(properties):
-        print(f"Cell nr. {cellid}, area: {region.area}")
-    
+    areas = pd.DataFrame(properties)
+    print(areas)
+
+# %%
 # Run workflow on each image:
-nuclei_quantification(image_t1)
+threshold = 25
+nuclei_quantification(image_t1, napari_viewer, threshold)
 
 # Now try running the workflow using the second image!
