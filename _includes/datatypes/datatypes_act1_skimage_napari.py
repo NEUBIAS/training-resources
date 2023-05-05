a0c0cfa0dff28e788d2ba83755b38b10b5a67f01
Antonio Politi <mail@apoliti.de>
2023-05-05 12:15:19 +0200

    Fix link error and open_ij_tiff

--- _includes/datatypes/datatypes_act1_skimage_napari.py
+++ _includes/datatypes/datatypes_act1_skimage_napari.py
@@ -1,23 +1,29 @@
+# %% [markdown]
+# ## Explore image data types and value ranges
+
+# %%
 # Import modules
 import napari
 import numpy as np
+from OpenIJTIFF import open_ij_tiff
 
 # Open napari
 viewer = napari.Viewer()
 
+# %%
 # Open the image
-# - Uncomment the image that you want to read
-from skimage.io import imread
-image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_noisy_different_intensity.tif')
-#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_a.tif')
-#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_b.tif')
-#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__two_objects.tif')
-#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__autophagosomes.tif')
-#image = imread('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__scanR_datatype_issue.tif')
+# Execute one of the cells 
+image, _, _, _ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_noisy_different_intensity.tif')
+#image, _, _, _ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_a.tif')
+#image, _, _, _ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_b.tif')
+#image, _, _, _ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__h2b.tif')
+#image, _, _, _ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__autophagosomes.tif')
+#image, _, _, _ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_16bit__scanR_datatype_issue.tif')
 
 # View the image
 viewer.add_image(image)
 
+# %%
 # Check the image's datatype and its value limits
 print(np.iinfo(image.dtype))
 
