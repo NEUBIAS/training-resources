68ab9f8922a32fad670a7b6c2fe7d8d77a71a8f4
Nima Vakili <63456510+nima-vakili@users.noreply.github.com>
2023-05-25 11:47:54 +0200

    Update median_filter_skimage_napari.py

--- _includes/median_filter/median_filter_skimage_napari.py
+++ _includes/median_filter/median_filter_skimage_napari.py
@@ -13,7 +13,6 @@ viewer = napari.Viewer()
 from OpenIJTIFF import open_ij_tiff
 image1, axes1, scales1, units1 = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__squares_different_size.tif')
 
-
 # Inspect image data type and values
 print('image type:', image1.dtype,'\n',
       'image shape:', image1.shape,'\n',
@@ -26,17 +25,28 @@ viewer.add_image(image1, name='original image1')
 
 # First method using the median filter
 from skimage import filters
+# First method using the median filter
+from skimage.filters import rank
+
 from skimage.morphology import disk
 
 # Local median filtering with radius 1
-Median_1 = filters.median(image1, disk(1))
-viewer.add_image(Median_1, name='Median1')
+median_1 = filters.median(image1, disk(1))
+viewer.add_image(median_1, name='Median1')
+# Local mean filtering with radius 1
+mean_1 = rank.mean(image1, disk(1))
+viewer.add_image(mean_1, name='Mean1')
 
 # Local median filtering with radius 2
 Median_2 = filters.median(image1, disk(2))
 viewer.add_image(Median_2, name='Median2')
+# Local mean filtering with radius 2
+mean_2 = rank.mean(image1, disk(2))
+viewer.add_image(mean_2, name='Mean2')
 
 # Local median filtering with radius 5
 Median_3 = filters.median(image1, disk(5))
 viewer.add_image(Median_3, name='Median3')
-
+# Local mean filtering with radius 5
+mean_3 = rank.mean(image1, disk(5))
+viewer.add_image(mean_3, name='Mean3')
