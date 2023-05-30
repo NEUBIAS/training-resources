99d1017ef6dfad0300f201fa9fcbf378a90060ad
Christian Tischer <christian.tischer@embl.de>
2023-05-30 18:23:39 +0200

    Make channel image creation in act2 explicit

--- _includes/multidimensional_images/multidimensional_images_act2_skimage_napari.py
+++ _includes/multidimensional_images/multidimensional_images_act2_skimage_napari.py
@@ -4,7 +4,7 @@
 # %%
 # Load the image 
 from OpenIJTIFF import open_ij_tiff
-image, axes, scales, units  = open_ij_tiff("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyzc_8bit_beads_p_open.tif")
+image, axes, scales, units = open_ij_tiff("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyzc_8bit_beads_p_open.tif")
 
 # %%
 # Print image shape & axes
@@ -14,7 +14,7 @@ print("Scales:",scales)
 print("Units:",units)
 
 # %%
-# View the image with its scales
+# View the image
 from napari.viewer import Viewer
 napari_viewer = Viewer()
 napari_viewer.add_image(image, scale = scales)
@@ -24,7 +24,14 @@ napari_viewer.add_image(image, scale = scales)
 # **Napari GUI** Delete the image
  
 # %%
-# View image as separate channels
-scale_3D = [scales[0], scales[2], scales[3]] # We have one dimension less
-napari_viewer.add_image(image[:,0,:,:],  name = 'Ch1', colormap = 'magenta', scale = scale_3D)
-napari_viewer.add_image(image[:,1,:,:],  name = 'Ch2', colormap = 'green', blending='additive', scale = scale_3D)
+# Create images as separate channels
+scale_3D = [scales[0], scales[2], scales[3]] 
+image_ch0 = image[:,0,:,:]
+image_ch1 = image[:,1,:,:]
+print(image_ch0.shape)
+print(image_ch1.shape)
+
+# %%
+# View images as separate channels
+napari_viewer.add_image(image_ch0,  name = 'Ch1', colormap = 'magenta', scale = scale_3D)
+napari_viewer.add_image(image_ch1,  name = 'Ch2', colormap = 'green', blending='additive', scale = scale_3D)
