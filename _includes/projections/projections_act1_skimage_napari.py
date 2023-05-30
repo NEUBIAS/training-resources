3612d48d15b05f7abbbfce32c4b5512d107b5b86
Antonio Politi <mail@apoliti.de>
2023-05-30 10:34:48 +0200

    update activity and rename

--- _includes/projections/projections_act1_skimage_napari.py
+++ _includes/projections/projections_act1_skimage_napari.py
@@ -1,8 +1,5 @@
 # %% [markdown]
-# **Explore Max and Sum Projections** 
-#
-# To follow along you need to complete the tool \
-# installation activity for skimage napari.
+# ## Explore Max and Sum Projections
 #
 #
 
@@ -19,36 +16,52 @@ viewer = Viewer()
 
 # %%
 # Open a 3D image
-image, axes, scales, units = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyz_16bit__spots.tif')
+image_url = 'https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyz_16bit__spots.tif'
+image, axes, scales, units = open_ij_tiff(image_url)
 viewer.add_image(image)
-image.shape
-
-# %% [markdown]
-# Use Grid mode in **Napari-GUI** to view images side by side
 
 # %%
-# Remember the axis order 0=z, 1=x, 2=y
-# Maximum projection along z-axis
+# Check image dimension
+# Axis order is 0=z, 1=x, 2=y
+image.shape
+
+
+# %%
+# Maximum projection along z-axis. i.e. axis = 0
 max_z_image = np.max(image, axis=0)
 viewer.add_image(max_z_image)
+
+# %% [markdown]
+# Use *Toggle grid mode (Ctrl + G)* in **Napari-GUI** to view images side by side
 
 # %%
 # Sum projection along z-axis
 sum_z_image = np.sum(image, axis=0)
-viewer.add_image(sum_z_image)
-# Napari may have an issue displaying uint32. You may first need to convert to float
-# viewer.add_image(sum_z_image.astype(float), name = "sum_z_image_float") 
 
 
 # %%
 # Appreciate that changing the data type during sum projections is useful
 print(np.iinfo(image.dtype)) # range of values that the image's datatype can represent
-print("Maximum values possible during sum projection")
-print(image.shape[0]*np.iinfo(image.dtype).max) # maximum value that could occur during sum projection
+
+# %%
+# Maximum value that could occur during sum projection 
+# is number of slices (image.shape[0]) multiplied by the maximal possible value for each slice
+max_possible_value = image.shape[0]*np.iinfo(image.dtype).max
+print("Maximum possible pixel value during sum projection %d"  % max_possible_value)
 
 # %%
 # luckily numpy changed the data type during projection
 print(np.iinfo(sum_z_image.dtype)) # range of values that the new data type can represent
+
+# %%
+# Display the image in Napari
+viewer.add_image(sum_z_image)
+
+# %%
+# BUG - Napari has an issue displaying uint32. 
+# Convert to float to properly visualize the image
+sum_z_image_float = sum_z_image.astype(float)
+viewer.add_image(sum_z_image_float) 
 
 # %%
 # Maximum projection along x-axis and y-axis
@@ -74,7 +87,7 @@ rescaled_max_y_image = rescale(max_y_image, [dz/dy,1])
 viewer.add_image(rescaled_max_y_image)
 
 # %%
-# The max_x_image could be rescaled in the  same way
+# The max_x_image could be rescaled in the same way
 rescaled_max_x_image = rescale(max_x_image, [dz/dy,1])  
 viewer.add_image(rescaled_max_x_image)
 
