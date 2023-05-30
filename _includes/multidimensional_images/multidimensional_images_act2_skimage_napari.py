8ef24c319f644cd71caa90b8570e3eec0f2b2091
Christian Tischer <christian.tischer@embl.de>
2023-05-30 18:02:45 +0200

    Slight modifications of act2

--- _includes/multidimensional_images/multidimensional_images_act2_skimage_napari.py
+++ _includes/multidimensional_images/multidimensional_images_act2_skimage_napari.py
@@ -1,39 +1,30 @@
 # %% [markdown]
-# ## Explore dimensions in a 4D image (3D + channels)
+# ## Explore a 3D multi-channel image
 
 # %%
+# Load the image 
 from OpenIJTIFF import open_ij_tiff
-# Load the image
-# You can also load a local image by providing the path to the file
 image, axes, scales, units  = open_ij_tiff("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xyzc_8bit_beads_p_open.tif")
 
+# %%
+# Print image shape & axes
+print("Shape:", image.shape)
+print("Axes:",axes)
+print("Scales:",scales)
+print("Units:",units)
+
+# %%
+# View the image with its scales
 from napari.viewer import Viewer
-# Create a new napari viewer.
 napari_viewer = Viewer()
-
-# %%
-# Print image shape
-print(image.shape)
-
-# %%
-# Print axes IDs
-print(axes)
-
-# %%
-# Print scaling
-print(scales)
-
-# %%
-# Add an image to the napari_viewer.
 napari_viewer.add_image(image, scale = scales)
 
 # %% [markdown]
 # **Napari GUI** Explore different sliders and values in the bottom left part \
-# **Napari GUI** Show in 3D. Realize that Z and C axis are swapped
 # **Napari GUI** Delete the image
-
+ 
 # %%
-# Load image with correct channel order
+# View image as separate channels
 scale_3D = [scales[0], scales[2], scales[3]] # We have one dimension less
 napari_viewer.add_image(image[:,0,:,:],  name = 'Ch1', colormap = 'magenta', scale = scale_3D)
 napari_viewer.add_image(image[:,1,:,:],  name = 'Ch2', colormap = 'green', blending='additive', scale = scale_3D)
