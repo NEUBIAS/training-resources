81b78c043dbcace373ce2e0f24849954e440b44a
grinic <45073015+grinic@users.noreply.github.com>
2023-05-30 10:47:53 +0200

    update activity

--- _includes/spatial_calibration/spatial_calibration_act1_skimage_napari.py
+++ _includes/spatial_calibration/spatial_calibration_act1_skimage_napari.py
@@ -27,9 +27,9 @@ napari_viewer = Viewer()
 napari_viewer.add_image(image, scale=voxel_size_input)
 
 # %% [markdown]
-# **Napari GUI** using the orthogonal views button (`Change order of visible axes`) \
-# **Napari GUI** inspect the volume and observe that the voxel size makes sense.\
-# **Napari GUI** use 3D viewer button to inspect data in 3D (`Toggle ndisplay`).
+# - **Napari GUI** using the orthogonal views button (`Change order of visible axes`)
+# - **Napari GUI** inspect the volume and observe that the voxel size makes sense.
+# - **Napari GUI** use 3D viewer button to inspect data in 3D (`Toggle ndisplay`).
 
 # %%
 # update voxel size to some other values
@@ -47,50 +47,36 @@ save_ij_tiff(
 )
 
 # %% [markdown]
-# Add an image with changed scale. \
-# Visualize images side by side in Napari (`toggle grid mode`)
+# - Add an image with changed scale.
+# - Visualize images side by side in Napari (`toggle grid mode`)
 
 # %%
 napari_viewer.add_image(image, scale=voxel_size_output, name = "rescaled")
 
 # %% [markdown]
-# **Napari GUI** use the `New points layer button` to create a new point layer and name it `points2D` (double click on the name to rename a layer) \
-# **Napari GUI** use `Add points` to create 2 points in the 2D slice \
-# **Napari GUI** do the same for points in 3D in a separate layer called `points3D`
+# - **Napari GUI** use the `New points layer button` to create a new point layer
+# - **Napari GUI** use `Add points` to create 2 points (in the same plane or in different planes)
 
 # %%
 # extract point coordinates
-points2d = napari_viewer.layers['points2D'].data
-points3d = napari_viewer.layers['points3D'].data
+points = napari_viewer.layers['Points'].data
 
 # %%
-# compute distance between 2D points in voxel indices
-dist_2d_pxl = np.sqrt(((points2d[1]-points2d[0])**2).sum())
-print('Distance in pixels:',dist_2d_pxl)
+# compute distance between points in voxel indices
+dist_pxl = np.sqrt(((points[1]-points[0])**2).sum())
+print('Distance in pixels:',dist_pxl)
 
 # %%
-# calibrate point position and compute distance in um 
-points2d_cal = np.stack([p*voxel_size_input for p in points2d])
-# compute distance between points in um using calibrated point positions, appreciate that these are different values!
-dist_2d_cal = np.sqrt(((points2d_cal[1]-points2d_cal[0])**2).sum())
-print('Distance in um:',dist_2d_cal)
-
-# appreciate that, in this special case, one can use voxel-distance multiplied by XY pixel size:
-print('Distance in um:',dist_2d_pxl * voxel_size_input[1])
+# extract point calibrated positions
+points_cal = napari_viewer.layers['Points'].data * napari_viewer.layers['Points'].scale
 
 # %%
-# compute distance between 3D points in voxel indices
-dist_3d_pxl = np.sqrt(((points3d[1]-points3d[0])**2).sum())
-print('Distance in pixels:',dist_3d_pxl)
-# Appreciate that this distance doesn't make any physical sense due to the anisotropy of the image!
+# compute distance between points in um using calibrated point positions
+dist_cal = np.sqrt(((points_cal[1]-points_cal[0])**2).sum())
+print('Distance in um:',dist_cal)
 
-# %%
-# Instead, one should always measure distances between points using calibration!
-points3d_cal = np.stack([p*voxel_size_input for p in points3d])
-
-# %%
-# compute distance between points in um using calibrated point positions, appreciate that in this case it is important to do the calibration!
-dist_3d_cal = np.sqrt(((points3d_cal[1]-points3d_cal[0])**2).sum())
-print('Distance in um:',dist_3d_cal)
+# %% [markdown]
+# - discuss why dist_pxl differs from dist_cal
+# - appreciate that one needs to use spatial calibration to obtain distances in physical units
 
 # %%
