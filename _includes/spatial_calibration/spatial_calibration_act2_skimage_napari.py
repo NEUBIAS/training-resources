a171367dd0ac605cdc624b37e480f39d43633e31
Christian Tischer <christian.tischer@embl.de>
2023-06-05 15:19:34 +0200

    Improve spatial calibration activity 2

--- _includes/spatial_calibration/spatial_calibration_act2_skimage_napari.py
+++ _includes/spatial_calibration/spatial_calibration_act2_skimage_napari.py
@@ -4,6 +4,10 @@
 # - [skimage and napari](https://neubias.github.io/training-resources/tool_installation/index.html#skimage_napari)
 
 # %%
+import sys
+sys.path.append("/Users/tischer/Documents/training-resources/functions")
+
+# %%
 # Import python packages.
 from OpenIJTIFF import open_ij_tiff, save_ij_tiff
 import numpy as np
@@ -16,7 +20,7 @@ image_2D, axes_image_2D, voxel_size_image_2D, units_image_2D = open_ij_tiff(
 )
 
 # %%
-# Inspect the image axes metadata.
+# Inspect the image axes metadata
 print("Shape: ", image_2D.shape)
 print("Axes: ", axes_image_2D)
 print("Scale: ", voxel_size_image_2D)
@@ -39,7 +43,7 @@ print("Units: ", units_image_3D)
 # Note that the 3D image does not have calibrated metadata.
 # Let's add spatial calibration using the x&y voxel_size from the 2D image
 # and also add a z scaling 
-voxel_size_image_3D = [0.52, voxel_size_image_2D[0], voxel_size_image_2D[1]]
+voxel_size_image_3D = [0.52, voxel_size_image_2D[1], voxel_size_image_2D[0]]
 units_image_3D = ["um","um","um"]
 
 # %%
@@ -62,7 +66,7 @@ napari_viewer.add_image(image_3D, scale=voxel_size_image_3D, name='image_3D')
 # %%
 # Save the 3D image with the calibration metadata
 save_ij_tiff(
-    'image_3D_calibrated.tif',
+    '/Users/tischer/Desktop/image_3D_calibrated.tif',
      image_3D,
      axes_image_3D,
      voxel_size_image_3D,
