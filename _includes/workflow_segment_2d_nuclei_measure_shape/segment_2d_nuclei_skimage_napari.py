d6d4a09716c4c37d937659d6240c26fd26ba8d7e
Christian Tischer <christian.tischer@embl.de>
2023-06-07 14:07:13 +0200

    Improve segment_2d_nuclei_skimage_napari.py

--- _includes/workflow_segment_2d_nuclei_measure_shape/segment_2d_nuclei_skimage_napari.py
+++ _includes/workflow_segment_2d_nuclei_measure_shape/segment_2d_nuclei_skimage_napari.py
@@ -11,15 +11,15 @@ napari_viewer = napari.Viewer()
 
 # %%
 # Open and inspect the image
-# Learning opportunity: 
-# - change file_path and image name to analyse a different image
-#   - https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__mitocheck_incenp_t70.tif
+# Learning opportunity: change file_path and the name of the image to analyse a different image: https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__mitocheck_incenp_t70.tif
 file_path = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__mitocheck_incenp_t1.tif"
 image, axes, scales, units = open_ij_tiff(file_path)
 napari_viewer.add_image(image, name='incenp_t1')
 
 # %%
 # Binarize the image
+# Learning opportunity: explore different threshold values
+# - 
 image = image > 25
 viewer.add_labels(image, name='binary')
 
@@ -30,8 +30,7 @@ viewer.add_labels(img_labels, name='labels')
 
 # %%
 # Measure nuclei shapes
-# Learning opportunity: 
-# - add other measurements (Hint: google: skimage regionprops)
+# Learning opportunity: add other measurements (hint: google: skimage regionprops)
 properties = regionprops_table(
     img_labels,
     properties = {'label', 'area'}
@@ -39,7 +38,6 @@ properties = regionprops_table(
 
 # %%
 # Print areas for each cell
-# Learning opportunity: 
-# - print other measurements
+# Learning opportunity: print other measurements
 areas = pd.DataFrame(properties)
 print(areas)
