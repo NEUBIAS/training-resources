7542a83d09aa00d2180ab8be2ba238f50729c2a7
Antonio Politi <mail@apoliti.de>
2023-06-12 19:22:30 +0200

    image with 3 spots. Using single pixel spot

--- _includes/filter_morphological/filter_morphological_act1_skimage_napari.py
+++ _includes/filter_morphological/filter_morphological_act1_skimage_napari.py
@@ -1,9 +1,9 @@
-#######################################################
-## To follow along you need to complete the tool
-## installation activity for skimage napari.
-#######################################################
+# %% [markdown]
+# ### Dilation and erosion of binary
+#
+# Part of teaching module [Morphological filtering](https://neubias.github.io/training-resources/filter_morphological/#dilateerode)
 
-#%%
+# %%
 # Import python packages.
 from OpenIJTIFF import open_ij_tiff
 from napari.viewer import Viewer
@@ -11,84 +11,58 @@ from skimage.morphology import square, disk
 from skimage.morphology import erosion, dilation
 from skimage.morphology import opening, closing
 
-#%% 
-# download and read tif file
-fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__two_spots_different_size.tif"
-image_2spots, _, _, _ = open_ij_tiff(fpath)
-
 # Create a napari_viewer and visualize image and labels.
 napari_viewer = Viewer()
-napari_viewer.add_image(image_2spots, name='image')
 
-#%%
-# Perform erosion and dilation with default structuring element (cross, connectivity=1).
-eroded = erosion(image_2spots)
-dilated = dilation(image_2spots)
+# %%
+# download and read tif file
+fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__three_spots_different_size.tif"
+image, _, _, _ = open_ij_tiff(fpath)
 
-#%%
-# Now try with connectivity 2 (square).
+napari_viewer.add_image(image, name='image')
+
+# %% [markdown]
+# Perform [erosion](https://scikit-image.org/docs/stable/api/skimage.morphology.html#skimage.morphology.erosion) and [dilation](https://scikit-image.org/docs/stable/api/skimage.morphology.html#skimage.morphology.dilation) using defaults
+#
+
+# %%
+# Perform erosion and dilation with default structuring element (cross-shaped)
+# 0 1 0 
+# 1 1 1
+# 0 1 0
+# This element has connectivity = 1
+eroded = erosion(image)
+dilated = dilation(image)
+
+# Add to napari
+napari_viewer.add_image(eroded, name='eroded1', colormap='magenta', opacity = 0.7)
+napari_viewer.add_image(dilated, name='dilated1', colormap = 'green', opacity = 0.7)
+
+# %% [markdown]
+# Appreciate that the single pixel disappeared with erosion.\
+# Appreciate that the single pixel became a cross with dilation. This is in fact the form of the structuring element
+
+# %%
+# Now try with a structuring element with connectivity 2 (3x3 square).
 square3 = square(3)
 print(square3)
-eroded_square3 = erosion(image_2spots, square3)
-dilated_square3 = dilation(image_2spots, square3)
+eroded_square3 = erosion(image, footprsquare3)
+dilated_square3 = dilation(image, square3)
 
-#%%
+napari_viewer.add_image(eroded_square3, name='eroded3', colormap='magenta', opacity = 0.5)
+napari_viewer.add_image(dilated_square3, name='dilated3', colormap='green', opacity = 0.5)
+
+# %%
 # add images to the viewer and inspect the results
 napari_viewer.add_image(eroded, name='eroded1')
 napari_viewer.add_image(dilated, name='dilated1')
 napari_viewer.add_image(eroded_square3, name='eroded3')
 napari_viewer.add_image(dilated_square3, name='dilated3')
 
-# Now try with bigger square (e.g. square(5))
-# or a different structuring element (e.g. disk(1))
-# Also refer to:
-# https://scikit-image.org/docs/stable/api/skimage.morphology.html
+# %% [markdown]
+# **Learning opportunity**\
+# Try with a bigger square (e.g. `square(5)`)\
+# or a different structuring element (e.g. disk(1))\
+# Also refer to https://scikit-image.org/docs/stable/api/skimage.morphology.html
 
-#%%
-# Explore opening and closing combining erosion and dilation.
-# Verify that they give the same results.
-fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__for_open_and_close.tif"
-image_structures, _, _, _ = open_ij_tiff(fpath)
-
-#%%
-# Opening: remove thin structures, smoothen objects.
-opening_manual = dilation(erosion(image_structures, square(3)), square(3))
-opening_1step = opening(image_structures, square(3))
-print((opening_manual==opening_1step).all())
-
-#%%
-# Closing: fill holes, connect gaps.
-closing_manual = erosion(dilation(image_structures, square(3)), square(3))
-closing_1step = closing(image_structures, square(3))
-print((closing_manual==closing_1step).all())
-
-#%%
-# Try using the default structuring element (disk(1)).
-# Explain what has changed and why.
-closing_disk = closing(image_structures, disk(1))
-
-#%%
-# Create a napari_viewer and visualize images.
-napari_viewer = Viewer()
-napari_viewer.add_image(image_structures, name='image')
-napari_viewer.add_image(opening_1step, name='opening')
-napari_viewer.add_image(closing_1step, name='closing')
-napari_viewer.add_image(closing_disk, name='closing_disk')
-
-#%%
-# Explore internal gradient.
-fpath = "https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit_binary__h2b.tif"
-image, _, _, _ = open_ij_tiff(fpath)
-
-# Internal gradient is the difference between the image and the eroded version of it.
-eroded = erosion(image)
-internal_gradient = image-eroded
-
-#%%
-# Create a napari_viewer and visualize images.
-napari_viewer = Viewer()
-napari_viewer.add_image(image, name='image')
-napari_viewer.add_image(internal_gradient, name='internal_gradient')
-
-# The internal gradient represents the edge of the object.
-# Discuss when and how this can be useful.
+# %%
