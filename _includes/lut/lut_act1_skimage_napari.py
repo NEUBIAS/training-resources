134410e41f7db8913aa6f4b5111a0e9e2ed5fa53
Christian Tischer <christian.tischer@embl.de>
2024-04-27 16:13:48 +0200

    Improve the python LUT activities

--- _includes/lut/lut_act1_skimage_napari.py
+++ _includes/lut/lut_act1_skimage_napari.py
@@ -1,5 +1,5 @@
-# %% [markdown]
-# # Explore LUTs
+# %%
+# Using different Lookup Tables (LUTs) in napari
 
 # %%
 # Instantiate the napari viewer
@@ -7,50 +7,30 @@ import napari
 viewer = napari.Viewer()
 
 # %%
-# Read the image
+# Read an image and its metadata
 from OpenIJTIFF import open_ij_tiff
-image, axes, scales, units = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_high_dynamic_range.tif')
-
-# %% [markdown]
-# ### Napari GUI alternative to load data
-# Drag and drop and rename the layer (alternative for loading data)\
-# Change name of layer `viewer.layers[0].name = 'image_grayscale'` \
-# Get the data as numpy array `image = viewer.layers['image_grayscale'].data` 
-
-# %%
-# Check image type and values
-import numpy as np
-print(image.dtype, np.min(image), np.max(image))
+image, axes, scales, units = open_ij_tiff("https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_high_dynamic_range.tif")
 
 # %%
 # View the intensity image as grayscale
 viewer.add_image(image, name='image_grayscale', colormap='gray')
 
 # %%
-# Change brightness and contrast
+# Change contrast programatically
+# Jupyter: Use TAB to to practice auto-completion when entering below command
 viewer.layers['image_grayscale'].contrast_limits=(100, 175)
 
-# %% [markdown]
-# **Napari GUI** explore different contrast limits
+# %%
+# Napari: Change the contrast in the GUI
+# Napari: Right click on "contrast limits" to see the value range 
 
 # %%
-# View the intensity image as grayscale
+# Add the image a second time
 viewer.add_image(image, name='image_grayscale2', colormap='gray')
 
-# %% [markdown]
-# **Napari GUI** visualize images side by side\
-# **Napari GUI** change brightness and contrast to visualize dim nuclei
-
 # %%
-# Check available colormap
-print(list(napari.utils.colormaps.AVAILABLE_COLORMAPS))
-
-# %%
-# Change colormap
-viewer.add_image(image, name='image_turbo', colormap='turbo')
-
-# %% [markdown]
-# **Napari GUI** explore different LUTs
+# Napari: Visualize images side by side by toggling on the "Grid mode"
+# Napari: Change contrast in one image to see the dim nuclei
 
 # %%
 # Extract image data from the layers
@@ -58,8 +38,21 @@ image_grayscale = viewer.layers['image_grayscale'].data
 image_grayscale2 = viewer.layers['image_grayscale2'].data
 
 # %%
-# Compare raw data
+# Check that changing the LUT did not change the data
 print(image_grayscale[0:5,0:5])
 print(image_grayscale2[0:5,0:5])
+
+# %%
+# Check whether all pixels of the two images are identical
 print((image_grayscale == image_grayscale2).all())
 
+# %%
+# Check available LUTs (colormaps)
+print(list(napari.utils.colormaps.AVAILABLE_COLORMAPS))
+
+# %%
+# Add the image again with a multi-color LUT
+viewer.add_image(image, name='image_turbo', colormap='turbo')
+
+# %% 
+# Napari: Explore different LUTs using the GUI
