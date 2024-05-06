46f907fa56c98dc552345b49e9e93f6bf3d2a779
Christian Tischer <christian.tischer@embl.de>
2024-05-06 14:14:41 +0200

    Improve local_background_correction_act1_skimage_napari.py

--- _includes/local_background_correction/local_background_correction_act1_skimage_napari.py
+++ _includes/local_background_correction/local_background_correction_act1_skimage_napari.py
@@ -1,63 +1,48 @@
-# %% [markdown]
-# ### Background subtraction using a median filter
-# From teaching module [Local background correction](https://neubias.github.io/training-resources/local_background_correction/index.html#bgmedian)
+# %% 
+# Background subtraction using a median filter
 
 # %%
+# import modules
 import numpy as np
-
-# Instantiate the napari viewer
 import napari
-viewer = napari.Viewer()
-
-# Read the intensity image
 from OpenIJTIFF import open_ij_tiff
-image, axes, scales, units = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__some_spots_with_uneven_bg.tif')
-
-# View the intensity image
-viewer.add_image(image, name='original image')
-
-# Inspect image data type and values
-print('image type:', image.dtype,'\n',
-      'image shape:', image.shape,'\n',
-      'intensity min:',   np.min(image),'\n',
-      'intensity max:',   np.max(image),'\n'
-      )
-
-# %% [markdown]
-# Compute background image with the median filter from skimage
-# %%
 from skimage import filters
 from skimage.morphology import disk
 
-# Median filtering with radius 15
-# Structuring element - aka footprint is disk
-background = filters.median(image, disk(15))
-viewer.add_image(background, name='background')
-
-# %% [markdown]
-# Compute a foreground image by subtracting the background image from the raw image
+# %%
+# Instantiate the napari viewer
+viewer = napari.Viewer()
 
 # %%
-# Convert/cast to signed int16 and subtract. 
-# A subtraction can cause negative values and we would like to keep these values
+# Read the image
+image, *_ = open_ij_tiff('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__some_spots_with_uneven_bg.tif')
+
+# %%
+# View the image
+# - Appreciate that due to the strong uneven background it is impossible to segment the spots with a simple threshold
+viewer.add_image(image, name='original image')
+
+# %% 
+# Compute background image using a large median filter to remove the small foreground objects
+background = filters.median(image, disk(15))
+viewer.add_image(background)
+
+# %%
+# Compute a foreground image by subtracting the background image from the raw image
+# - Convert to signed int16, because a subtraction can cause negative values, which we would like to keep
+print(image.dtype, image.min(), image.max())
 foreground = image.astype('int16') - background.astype('int16')
+print(foreground.dtype, foreground.min(), foreground.max())
+
+# %% 
+# Add the image to napari and 
+# inspect the intensity image values in order to identify a threshold
+# that segments the foreground dots
 viewer.add_image(foreground, name='foreground')
 
-# %% [markdown]
-# **Napari GUI** \
-# Inspect the intensity image values in order to identify a threshold
-# that segments cells \
-# Hover with mouse, line profile
-
 # %%
-# Threshold the image
-binary_image_cells = foreground > 8
+# Threshold the foreground image
+binary_image_spots = foreground > 8
 # Overlay the binary image
-viewer.add_image(binary_image_cells, name='segmented image')
+viewer.add_image(binary_image_spots)
 
-
-# %% [markdown]
-# ### Learning opportunity: Effect of radius on local background subtraction
-# Modify radius of median filter and compute a new foreground image\
-# For example try a radius of 3 and of 50\
-# Discuss the different results
