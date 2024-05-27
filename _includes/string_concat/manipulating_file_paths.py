d0ad0a246427f0fc8c00f2bd4dd62fb7aeb7d62f
k-dominik <k-dominik@users.noreply.github.com>
2024-05-27 19:16:31 +0200

    Added basic implementation for string and path manipulation in python

--- _includes/string_concat/manipulating_file_paths.py
+++ _includes/string_concat/manipulating_file_paths.py
@@ -1,1 +1,14 @@
-# TODO
+# manipulating file paths
+
+import pathlib
+import tempfile
+
+# Temporary folder is different on different operating systems.
+# We use tempfile.gettempdir() from the standard library as it has all that logic.
+tmp_folder = pathlib.Path(tempfile.gettempdir())
+file_name = "nuclei.tif"
+
+# pathlib.Path objects support "syntactic sugar", like using '/' as a
+# separator when composing paths.
+path = tmp_folder / file_name
+print(path)
