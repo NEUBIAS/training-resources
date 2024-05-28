82e50b87cf9ccf6410c959d8ee727b2f5fbe9ecd
k-dominik <k-dominik@users.noreply.github.com>
2024-05-28 09:38:17 +0200

    review suggestions
    * group code sections with `# %%` (will be separate cells in notebooks)
    * more meaningful comments
    
    Co-authored-by: Christian Tischer <christian.tischer@embl.de>

--- _includes/string_concat/creating_log_messages.py
+++ _includes/string_concat/creating_log_messages.py
@@ -1,11 +1,14 @@
-# creating log messages
+# %%
+# Creating log messages
 
 image_index = 11
 num_images = 100
 
+# %%
 # strings can be added together with the '+' operator
 print("Analyzing image " + str(image_index) + "/" + str(num_images) + "...")
 
+# %%
 # An alternative way to combine strings and variables from your code
 # are so called f-strings: in f-strings you can add variables (and some
 # code) in between curly braces. Types are automatically coerced to strings.
--- _includes/string_concat/manipulating_file_paths.py
+++ _includes/string_concat/manipulating_file_paths.py
@@ -1,13 +1,18 @@
-# manipulating file paths
+# %%
+# Manipulating file paths
 
 import pathlib
 import tempfile
 
+# %%
+# Let's get a folder and a file name to practice with.
 # Temporary folder is different on different operating systems.
 # We use tempfile.gettempdir() from the standard library as it has all that logic.
 tmp_folder = pathlib.Path(tempfile.gettempdir())
 file_name = "nuclei.tif"
 
+# %%
+# Combine folder and file name.
 # pathlib.Path objects support "syntactic sugar", like using '/' as a
 # separator when composing paths.
 path = tmp_folder / file_name
