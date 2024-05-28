b3179f1f55490c65003e91095263b853f7faf0cd
k-dominik <k-dominik@users.noreply.github.com>
2024-05-28 10:19:10 +0200

    Add more types of path manipulations to python activity

--- _includes/string_concat/manipulating_file_paths.py
+++ _includes/string_concat/manipulating_file_paths.py
@@ -15,5 +15,24 @@ file_name = "nuclei.tif"
 # Combine folder and file name.
 # pathlib.Path objects support "syntactic sugar", like using '/' as a
 # separator when composing paths.
-path = tmp_folder / file_name
-print(path)
+image_path = tmp_folder / file_name
+print(image_path)
+
+# %%
+# The parent for a Path object gives you the containing folder
+parent_folder = image_path.parent
+print(parent_folder == tmp_folder)
+
+# %%
+# Let's create a file name for a label image derived from the nuclei.tif
+# such the file name will be nuclei_labels.tif.
+filename_no_extension = image_path.stem
+print(filename_no_extension)
+labels_path = tmp_folder / f"{filename_no_extension}_labels.tif"
+
+# %%
+# Now we want to create a file name for a .csv file, that shares the
+# file name up to the extension.
+# If we only change the extension, we can use the with_suffix method.
+csv_path = image_path.with_suffix(".csv")
+print(csv_path)
