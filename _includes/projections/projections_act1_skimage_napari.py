4d8940665ed92e182137005b3c3bba358d12be0f
Christian Tischer <christian.tischer@embl.de>
2024-06-30 17:09:16 +0200

    Further improve the projections skimage napari implementation

--- _includes/projections/projections_act1_skimage_napari.py
+++ _includes/projections/projections_act1_skimage_napari.py
@@ -18,16 +18,13 @@ print(units)
 
 # %%
 # As this is an anisotropic image, we view it scaled
+# - Napari: Use 3-D rendering see all the spots
 viewer = Viewer()
 viewer.add_image(image, scale=scales)
 
 # %%
-# Napari:
-# - View the image also in 3-D to see all the spots
-
-# %%
 # Create and view a maximum projection along z-axis, i.e. axis = 0
-# - Observe that this gives a nice "quick overview" of the data content
+# - Observe how the maximum gives a nice "quick overview" of the data content
 # - In order to see it at the same scale as the original image we need to scale in in x&y 
 # - Napari: observe that the image layer context menu also allows one to create projections
 # - Napari: *Toggle grid mode (Ctrl + G)* to view images side by side
@@ -37,8 +34,9 @@ viewer.add_image(max_z_image, scale=scales[1:3])
 # %%
 # Compute sum projection along z-axis
 # and display it in napari
-# - Napari: Observe a current BUG where it does not properly render the sum projection pixel values;
-#   a workaround will be given later (see below)
+# - Napari: There is an an issue rendering the sum projection pixel values:
+#   some high values appear dark. More details and a possible work-around is given at 
+#   the end of this script.
 sum_z_image = np.sum(image, axis=0)
 viewer.add_image(sum_z_image, scale=scales[1:3])
 
@@ -61,15 +59,16 @@ print("actual max sum in this image:", sum_z_image.max())
 print("max sum supported by projection:", np.iinfo(sum_z_image.dtype).max)
 
 # %%
-# Optional workaround for the above mentioned BUG:
-# Convert sum projection to float to properly visualize it
-sum_z_image_float = sum_z_image.astype(float)
-viewer.add_image(sum_z_image_float, scale=scales[1:3]) 
-
-# %%
 # Compute maximum projection along x-axis and y-axis
 # and show in viewer with correct scaling
 max_x_image = np.max(image, axis=2)
 max_y_image = np.max(image, axis=1)
 viewer.add_image(max_x_image, scale=[scales[0], scales[1]])
 viewer.add_image(max_y_image, scale=[scales[0], scales[2]])
+
+# %%
+# Optional work-around for the above mentioned issue when rendering
+# certain data types (https://github.com/NEUBIAS/training-resources/issues/570)
+# Convert sum projection to float to properly visualize it
+sum_z_image_float = sum_z_image.astype(float)
+viewer.add_image(sum_z_image_float, scale=scales[1:3]) 
