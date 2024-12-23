8880cfca95d7c33ff2624adffdfe83cd06dad870
Felix Schneider <felix.schneider1@embl.de>
2024-12-23 09:53:01 +0100

    Removed in direct conversion

--- _includes/ome_tiff/convert_lif_to_ome_tif_bioio.py
+++ _includes/ome_tiff/convert_lif_to_ome_tif_bioio.py
@@ -13,42 +13,9 @@ image_url = "https://github.com/NEUBIAS/training-resources/raw/master/image_data
 bioimage = BioImage(image_url)
 
 # %%
-# Option 1: Direct conversion
+# Option 1:
 from pathlib import Path
 bioimage.save(Path().cwd()/'converted_file.ome.tiff')
 
-# Alternative
+# Option 2:
 BioImage(image_url).save(Path().cwd()/'direct_conversion.ome.tiff')
-
-# %%
-# Option 2: Indirect conversion
-# Single image to OME Tiff
-from bioio.writers import OmeTiffWriter
-
-# Extract image and pixel info
-bioimage.set_scene(0)
-image1 = bioimage.data.squeeze()
-pixel_info1 = bioimage.physical_pixel_sizes
-
-# Perform some modification/analysis/...
-
-# Save as OME Tiff
-OmeTiffWriter.save(data=image1, # image to save
-                   uri=Path().cwd()/'ConvertSingleImage.ome.tiff', # path to save to
-                   physical_pixel_sizes = pixel_info1, 
-                   dim_order='CYX') # dimension order
-
-# %%
-# Two images
-# Extract image and pixel info
-bioimage.set_scene(1)
-image2 = bioimage.data.squeeze()
-pixel_info2 = bioimage.physical_pixel_sizes
-
-# Perform some modification/analysis/...
-
-# Save as OME Tiff
-OmeTiffWriter.save(data=[image1,image2],
-                   uri=Path().cwd()/'ConvertTwoImages.ome.tiff',
-                   dim_order=['CYX','YX'],
-                   physical_pixel_sizes=[pixel_info1,pixel_info2])
