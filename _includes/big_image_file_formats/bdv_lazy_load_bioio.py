d62759f59dbd26e0d0e4cdd45f191c4a995ccadc
Felix Schneider <felix.schneider1@embl.de>
2025-02-27 12:38:22 +0100

    Add inspection of different resolution layers

--- _includes/big_image_file_formats/bdv_lazy_load_bioio.py
+++ _includes/big_image_file_formats/bdv_lazy_load_bioio.py
@@ -3,7 +3,7 @@
 # conda env for this module
 # conda create -n bio_io_bdv python=3.10
 # activate bio_io_bdv
-# pip install bioio bioio-tifffile
+# pip install bioio bioio-bioformats
 
 # %%
 # Load BDV file
@@ -26,3 +26,21 @@ print(image_data)
 #%%
 # lazy load a specific image plane
 bioimage_data = bioimage.dask_data[:,:,:,10,:].compute()
+
+#%%
+# Checkout different resolution levels
+# The way it should be implemented and according to docs
+# Get current resolution level
+print(bioimage.current_resolution_level)
+# See all resolution levels
+print(bioimage.resolution_levels)
+# Set resolution level
+bioimage.set_resolution_level(0)
+
+# The way how it is actually working for this type of image:
+# Get current resolution level
+print(bioimage.current_scene) # Prints string as combination of image name and resolution level
+# See all resolution levels
+print(bioimage.scenes)
+# Set resolution level
+bioimage.set_scene(0) # or for scene in bioimage.scenes: bioimage.set_scene(scene)
