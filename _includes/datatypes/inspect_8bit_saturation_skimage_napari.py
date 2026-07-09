716d0e6898dc3b7884c6fb3d16407e12144d173b
Felix Schneider <felix.schneider1@embl.de>
2026-07-09 10:36:57 +0200

    Include bioio for reading image and add "HiLo" colormap as suggested in TODO

--- _includes/datatypes/inspect_8bit_saturation_skimage_napari.py
+++ _includes/datatypes/inspect_8bit_saturation_skimage_napari.py
@@ -12,27 +12,25 @@ viewer = napari.Viewer()
 
 # %%
 # Open an image and view it
-image = BioImage('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_a.tif')
-image = image.data.squeeze()
-viewer.add_image(image)
-
-# TODO: This would be nice https://forum.image.sc/t/add-hilo-colormap-to-napari/95601
+image_object = BioImage('https://github.com/NEUBIAS/training-resources/raw/master/image_data/xy_8bit__nuclei_intensity_clipping_issue_a.tif')
+image_data = image_object.data.squeeze()
+viewer.add_image(image_data,colormap='HiLo')
 
 # %% 
 # Check the image's datatype
-print(image.dtype)
-print(np.iinfo(image.dtype)) # Useful as it also prints the value range
+print(image_data.dtype)
+print(np.iinfo(image_data.dtype)) # Useful as it also prints the value range
 
 # %%
 # Check for clipping, i.e. pixels values at the limits of the value range
 # This is important for many reasons, for example: 
 # - Pixel values at the limit of the value range typically cannot be used for intensity quantification 
 # - Important algorithms, e.g. for spot detection, do not work well in regions with intensity clipping
-print("Min:", image.min()) # Are there any clipped pixels?
-print("Max:", image.max()) # Are there any clipped pixels?
-print("Number of 0 pixels:", np.sum(image==0)) # How many clipped pixels are there?
-print("Number of 255 pixels:", np.sum(image==255))
-plt.hist(image.flatten(), bins=np.arange(int(image.min()), int(image.max()) + 1).tolist())
+print("Min:", image_data.min()) # Are there any clipped pixels?
+print("Max:", image_data.max()) # Are there any clipped pixels?
+print("Number of 0 pixels:", np.sum(image_data==0)) # How many clipped pixels are there?
+print("Number of 255 pixels:", np.sum(image_data==255))
+plt.hist(image_data.flatten(), bins=np.arange(int(image_data.min()), int(image_data.max()) + 1).tolist())
 plt.show()
 
 # %% 
